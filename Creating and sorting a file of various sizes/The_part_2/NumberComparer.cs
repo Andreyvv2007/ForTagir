6338ee97ef46d78fb50e0e98fa91ebf9d5af40e5
Andreyvv2007 <tovkes.a@gmail.com>
2023-11-23 12:55:30 +0300

    Creating and sorting a file of various sizes
    Creating and sorting a file of various sizes

---
+++ Creating and sorting a file of various sizes/The_part_2/NumberComparer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace The_part_2
+{
+    internal class NumberComparer : IComparer<NameString>
+    {
+        public static int NumerReturn { get; private set; }
+
+        public int Compare(NameString n1, NameString n2)
+        {
+            if (n1.Number > n2.Number)
+            {
+                NumerReturn = 1;
+                return 1;
+            }
+            else if (n1.Number < n2.Number)
+            {
+                NumerReturn = -1;
+                return -1;
+            }
+            else
+            {
+                NumerReturn = 0;
+                return 0;
+            }
+        }
+    }
+}
---
+++ Creating and sorting a file of various sizes/The_part_2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Data.SqlTypes;
+using System.Management;
+using System.Numerics;
+using System.Text;
+using System.Xml.Linq;
+using The_part_2;
+
+FileManager.CreatTempFiles();
+
+MergeSort.MergeSortAndMergeDocuments();
+
+Console.WriteLine("************");
+Console.WriteLine("* The End! *");
+Console.WriteLine("************");
+Console.ReadLine();
+
