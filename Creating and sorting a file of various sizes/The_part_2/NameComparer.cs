42ffd847c3eaab4425b99379a65bfa0743682952
Andreyvv2007 <tovkes.a@gmail.com>
2023-11-23 13:09:00 +0300

    Creating and sorting a file of various sizes
    Creating and sorting a file of various sizes

---
+++ Creating and sorting a file of various sizes/The_part_2/NameComparer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace The_part_2
+{
+    internal class NameComparer : IComparer<NameString>
+    {
+        public int Compare(NameString n1, NameString n2)
+        {
+            int cmpVal = n1.Name.CompareTo(n2.Name);
+            NumberComparer comparer = new NumberComparer();
+
+            if (cmpVal > 0)
+            {
+                return 1;
+            }
+            else if (cmpVal < 0)
+            {
+                return -1;
+            }
+            else
+            {
+                comparer.Compare(n1, n2);
+                return NumberComparer.NumerReturn;
+            }
+        }
+    }
+}
